d122e91e0c6e5391a2b12e6afe7e43354af69ceb
orikarudo <okaccounts@gmail.com>
2019-02-18 11:58:20 -0500

    made some progress on compressor and fixed layout

--- app/src/main/java/com/example/a2048/MainActivity.java
+++ app/src/main/java/com/example/a2048/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import java.util.*;
+import java.lang.String;
 
 public class MainActivity extends AppCompatActivity {
     Button Up;
@@ -16,25 +18,12 @@ public class MainActivity extends AppCompatActivity {
     Button Left;
     Button Right;
 
-    Button A0 = (Button)findViewById(R.id.A1);
-    Button A1 = (Button)findViewById(R.id.A2);
-    Button A2 = (Button)findViewById(R.id.A3);
-    Button A3 = (Button)findViewById(R.id.A4);
-    Button B0 = (Button)findViewById(R.id.B1);
-    Button B1 = (Button)findViewById(R.id.B2);
-    Button B2 = (Button)findViewById(R.id.B3);
-    Button B3 = (Button)findViewById(R.id.B4);
-    Button C0 = (Button)findViewById(R.id.C1);
-    Button C1 = (Button)findViewById(R.id.C2);
-    Button C2 = (Button)findViewById(R.id.C3);
-    Button C3 = (Button)findViewById(R.id.C4);
-    Button D0 = (Button)findViewById(R.id.D1);
-    Button D1 = (Button)findViewById(R.id.D2);
-    Button D2 = (Button)findViewById(R.id.D3);
-    Button D3 = (Button)findViewById(R.id.D4);
+    List<Button> leftRow0;
 
-    Button[] allButtons = new Button[16];
 
+    /*public void myCompress(){
+
+    };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +36,40 @@ public class MainActivity extends AppCompatActivity {
         Down = (Button)findViewById(R.id.Down);
         Left = (Button)findViewById(R.id.Left);
         Right = (Button)findViewById(R.id.Right);
+
+        Button A0 = (Button)findViewById(R.id.A1);
+        Button A1 = (Button)findViewById(R.id.A2);
+        Button A2 = (Button)findViewById(R.id.A3);
+        Button A3 = (Button)findViewById(R.id.A4);
+        Button B0 = (Button)findViewById(R.id.B1);
+        Button B1 = (Button)findViewById(R.id.B2);
+        Button B2 = (Button)findViewById(R.id.B3);
+        Button B3 = (Button)findViewById(R.id.B4);
+        Button C0 = (Button)findViewById(R.id.C1);
+        Button C1 = (Button)findViewById(R.id.C2);
+        Button C2 = (Button)findViewById(R.id.C3);
+        Button C3 = (Button)findViewById(R.id.C4);
+        Button D0 = (Button)findViewById(R.id.D1);
+        Button D1 = (Button)findViewById(R.id.D2);
+        Button D2 = (Button)findViewById(R.id.D3);
+        Button D3 = (Button)findViewById(R.id.D4);
+
+        A0.setText("2");
+        A1.setText("4");
+        A2.setText("8");
+        A3.setText("16");
+
+        List<Button> allBtns = new ArrayList<Button>();
+        allBtns.add(0, A0);
+
+        List<Integer> testing = new ArrayList<Integer>();
+        testing.add(0, 44);
+
+        leftRow0 = new ArrayList<Button>();
+        leftRow0.add(0, A0);
+        leftRow0.add(1, A1);
+        leftRow0.add(2, A2);
+        leftRow0.add(3, A3);
 
         Up.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -72,11 +95,26 @@ public class MainActivity extends AppCompatActivity {
         Right.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                compressTiles(leftRow0);
 
             }
         });
 
 
+    }
+
+    public void compressTiles(List<Button> list)
+    {
+        int[] values = new int[4];
+        int num_non_zero = 0;
+        for (int i = 0; i < 4; ++i)
+        {
+            Button b = list.get(i);
+            CharSequence s = b.getText();
+            values[i] = (int) Integer.parseInt((String) s);
+            values[i] = values[i] + 10;
+            b.setText(String.format("%d", values[i]));
+        }
     }
 
     @Override
